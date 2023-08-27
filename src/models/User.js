5919047c4ce76a3b8c26dbf092096a33f75fb452
Gabriel Cardiano <gabriel.cardiano1@gmail.com>
2023-08-27 14:17:20 -0300

    fix: corrige model User

--- src/models/User.js
+++ src/models/User.js
@@ -8,7 +8,9 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
     password: DataTypes.STRING,
+    image: DataTypes.STRING
   }, {
     tableName: 'users',
     underscored: true,
