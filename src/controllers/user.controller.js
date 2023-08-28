7190457645ef9a471524619a2ab7947f618c58f6
Gabriel Cardiano <gabriel.cardiano1@gmail.com>
2023-08-28 14:42:12 -0300

    fix: req 4 - corrige validação de email (completo)

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,14 +5,13 @@ const userService = require('../services');
 async function CreateUser(req, res) {
   try {
     const { body } = req;
-
+    
     // verifica o email da requisição já existe no DB
     const isUser = await userService.getUserByEmail(body.email);
-    console.log('user>>>>', isUser);
     if (isUser) {
       return res.status(409).json({ message: 'User already registered' });
     }
-    
+
     // caminho feliz - requisição atende a todos os requisitos
     const user = await userService.createUser(body); // id e password
     const { password: _password, ...userWithoutPassword } = user.dataValues;
