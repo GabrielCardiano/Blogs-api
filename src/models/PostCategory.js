9e9214884feb0249f5836a0c020318ccb5efd754
Gabriel Cardiano <gabriel.cardiano1@gmail.com>
2023-08-29 20:37:39 -0300

    feat: req 11 - cria o model PostCategory

---
+++ src/models/PostCategory.js
@@ -0,0 +1,32 @@
+/**
+ * 
+ * @param {import('sequelize').Sequelize} sequelize 
+ * @param {import('sequelize')} DataTypes
+ */
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+    postId: { type: DataTypes.INTEGER, primaryKey: true, foreignKey: true },
+    categoryId: { type: DataTypes.INTEGER, primaryKey: true, foreignKey: true },
+  }, {
+    tableName: 'posts_categories',
+    underscored: true,
+    timestamps: true,
+  });
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+      as: 'categories'
+    });
+
+    models.Category.belongsToMany(models.BlogPost, {
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+      as: 'posts'
+    });
+  }
+  return PostCategory;
+};
